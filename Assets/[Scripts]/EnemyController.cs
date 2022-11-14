2e0edaac03f4b0d6942aaf8963547f3b5f272394
NotNitsua <aussio1234@gmail.com>
2022-11-13 23:14:51 -0500

    Added Simple enemy

---
+++ Assets/[Scripts]/EnemyController.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    [Header("Movement Properties")]
+    public float horizontalForce;
+
+    public Transform inFrontPoint;
+    public Transform aheadPoint;
+    public Transform groundPoint; //origin of circle
+    public float groundRadius; //size of circle
+    public LayerMask groundLayerMask; // stuff we can collide with
+    public bool isGrounded;
+    public bool isGroundAhead;
+    public bool isObstacleAhead;
+
+    public Vector2 direction;
+    void Start()
+    {
+        direction = Vector2.left;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isObstacleAhead = Physics2D.Linecast(groundPoint.position, inFrontPoint.position, groundLayerMask);
+        isGroundAhead = Physics2D.Linecast(groundPoint.position, aheadPoint.position, groundLayerMask);
+        isGrounded = Physics2D.OverlapCircle(groundPoint.position, groundRadius, groundLayerMask);
+
+        if(isGrounded && isGroundAhead)
+        {
+            Move();
+        }
+        if(!isGroundAhead || isObstacleAhead)
+        {
+            Flip();
+        }
+    }
+
+    public void Move()
+    {
+        transform.position += new Vector3(direction.x * horizontalForce * Time.deltaTime, 0.0f);
+    }
+    public void Flip()
+    {
+        var x = transform.localScale.x * -1.0f;
+        direction *= -1.0f;
+        transform.localScale = new Vector3(x, 1.0f, 1.0f);
+    }
+    public void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(groundPoint.position, groundRadius);
+        Gizmos.DrawLine(groundPoint.position, inFrontPoint.position);
+        Gizmos.DrawLine(groundPoint.position, aheadPoint.position);
+
+
+    }
+}
